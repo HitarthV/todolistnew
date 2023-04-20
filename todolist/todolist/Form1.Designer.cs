72345d0565627073974793702c9885fba00785eb
HitarthV <129500306+HitarthV@users.noreply.github.com>
2023-04-20 12:49:50 +0100

    Add files via upload

---
+++ todolist/todolist/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace todolist
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAddTask = new System.Windows.Forms.Button();
+            this.lstTasks = new System.Windows.Forms.ListBox();
+            this.btnMarkComplete = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(327, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TO DO LIST";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 188);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnAddTask
+            // 
+            this.btnAddTask.ForeColor = System.Drawing.Color.Blue;
+            this.btnAddTask.Location = new System.Drawing.Point(235, 147);
+            this.btnAddTask.Name = "btnAddTask";
+            this.btnAddTask.Size = new System.Drawing.Size(75, 23);
+            this.btnAddTask.TabIndex = 2;
+            this.btnAddTask.Text = "Add Task";
+            this.btnAddTask.UseVisualStyleBackColor = true;
+            this.btnAddTask.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstTasks
+            // 
+            this.lstTasks.FormattingEnabled = true;
+            this.lstTasks.Location = new System.Drawing.Point(330, 272);
+            this.lstTasks.Name = "lstTasks";
+            this.lstTasks.Size = new System.Drawing.Size(120, 95);
+            this.lstTasks.TabIndex = 3;
+            this.lstTasks.SelectedIndexChanged += new System.EventHandler(this.lstTasks_SelectedIndexChanged);
+            // 
+            // btnMarkComplete
+            // 
+            this.btnMarkComplete.ForeColor = System.Drawing.Color.ForestGreen;
+            this.btnMarkComplete.Location = new System.Drawing.Point(380, 147);
+            this.btnMarkComplete.Name = "btnMarkComplete";
+            this.btnMarkComplete.Size = new System.Drawing.Size(75, 23);
+            this.btnMarkComplete.TabIndex = 4;
+            this.btnMarkComplete.Text = "Completed";
+            this.btnMarkComplete.UseVisualStyleBackColor = true;
+            this.btnMarkComplete.Click += new System.EventHandler(this.btnMarkComplete_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.ForeColor = System.Drawing.Color.Red;
+            this.btnDelete.Location = new System.Drawing.Point(534, 147);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete Task";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnMarkComplete);
+            this.Controls.Add(this.lstTasks);
+            this.Controls.Add(this.btnAddTask);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAddTask;
+        private System.Windows.Forms.ListBox lstTasks;
+        private System.Windows.Forms.Button btnMarkComplete;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ todolist/todolist/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace todolist
+{
+    public partial class Form1 : Form
+    {
+        private List<string> tasks = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string task = textBox1.Text.Trim();
+            if (!string.IsNullOrEmpty(task))
+            {
+                tasks.Add(task);
+                lstTasks.Items.Add(task);
+                textBox1.Text = "";
+            }
+        }
+
+        private void btnMarkComplete_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex <= 0)
+            {
+                lstTasks.Items.RemoveAt(lstTasks.SelectedIndex);
+                lstTasks.ClearSelected();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex <= 0)
+            {
+                lstTasks.Items.RemoveAt(lstTasks.SelectedIndex);
+                lstTasks.ClearSelected();
+            }
+        }
+
+        private void lstTasks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex >= 0)
+            {
+                btnMarkComplete.Enabled = true;
+                btnDelete.Enabled = true;
+            }
+            else
+            {
+                btnMarkComplete.Enabled = false;
+                btnDelete.Enabled = false;
+            }
+        }
+    }
+}
